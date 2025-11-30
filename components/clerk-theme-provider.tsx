844a6102880e90d1237562c1ef469ffcca1dd5ac
faiz-oussama <faizouss123@gmail.com>
2025-11-30 16:18:37 +0100

    fix: added border for input fields for better UX

--- components/clerk-theme-provider.tsx
+++ components/clerk-theme-provider.tsx
@@ -19,7 +19,25 @@ export function ClerkThemeProvider({ children }: { children: ReactNode }) {
                     colorNeutral: '#e5e5e5',
                     borderRadius: '0.625rem',
                     fontFamily: 'var(--font-geist-sans), system-ui, sans-serif',
-                }
+                },
+                elements: {
+                    formFieldInput: {
+                        border: '1px solid #e5e5e5',
+                        '&:hover': {
+                            border: '1px solid #d4d4d4',
+                        },
+                        '&:focus': {
+                            border: '1px solid #212121',
+                            outline: 'none',
+                            boxShadow: '0 0 0 1px #212121',
+                        },
+                    },
+                    formButtonPrimary: {
+                        '&:hover': {
+                            backgroundColor: '#171717',
+                        },
+                    },
+                },
             }}
         >
             {children}
